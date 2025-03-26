8155234de301d6772ceeb781113dd590f10ae71c
Alam <alam.rool@gmail.com>
2025-03-25 21:06:53 -0600

    Se cambió la salida 'adios' por 'exit'

---
+++ dialog_engine.py
@@ -0,0 +1,37 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from menu_cafe import info 
+
+template = """
+You are a owner of a coffee shop and you need to answer information about our shop.
+
+Answer the question below always in Spanish in a friendly way.
+
+For example: If an user needs to know  how much is a Cofee, you need to anwer in a friendly way the costn and invite him to taste it.
+
+
+Here is the conversation history: {datos_negocio}
+
+Question: {question}
+
+Answer: 
+"""
+model = OllamaLLM(model="Gemma3:latest")
+prompt = ChatPromptTemplate.from_template(template)
+chain =prompt | model 
+
+def handle_conversation():
+    context = ""
+    print("=================¡Bienvenid@! Soy Icniuh, el asistente virtual de la UAM A. Escribe 'exit' para quitar=================")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+
+        result = chain.invoke({"datos_negocio":info, "context":context, "question":user_input})
+        print("Bot:", result)
+        context += f"\nUser: {user_input}\nAI: {result}"
+
+
+if __name__ == "__main__":
+    handle_conversation()
---
+++ menu_cafe.py
@@ -0,0 +1,41 @@
+info = """
+    "business_name": "Cafetería La Taza Feliz",
+    "address": "Calle Alegría 123, Ciudad de México",
+    "phone": "+52 55 1234 5678",
+    "hours": {
+        "monday_to_friday": "8:00 AM - 8:00 PM",
+        "saturday": "9:00 AM - 6:00 PM",
+        "sunday": "Cerrado"
+    },
+    "menu": {
+        "hot_drinks": [
+            {"name": "Café americano", "price": 30},
+            {"name": "Capuchino", "price": 40},
+            {"name": "Latte", "price": 45}
+        ],
+        "cold_drinks": [
+            {"name": "Frappé de chocolate", "price": 55},
+            {"name": "Frappé de vainilla", "price": 55}
+        ],
+        "snacks": [
+            {"name": "Panqué de plátano", "price": 25},
+            {"name": "Galleta de avena", "price": 20}
+        ]
+    },
+    "faqs": [
+        {
+            "question": "¿Tienen opciones sin azúcar?",
+            "answer": "Sí, contamos con endulzantes alternativos como stevia."
+        },
+        {
+            "question": "¿Cuentan con Wi-Fi?",
+            "answer": "Sí, el servicio de Wi-Fi es gratuito para nuestros clientes."
+        }
+    ],
+    "social_media": {
+        "facebook": "@LaTazaFeliz",
+        "instagram": "@latzafeliz",
+        "twitter": "@latzafelizmex"
+    },
+    "suggestions_email": "sugerencias@latzafeliz.com"
+"""
